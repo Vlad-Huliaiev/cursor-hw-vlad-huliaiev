53ef0392a575c19b050227c75009fe8ed795b019
Vlad Huliaiev <gulyaev4420@gmail.com>
2020-07-17 23:57:49 +0300

    correct let variables

--- 02-Homework/02-Homework.js
+++ 02-Homework/02-Homework.js
@@ -2,19 +2,19 @@
 document.write('<pre>');
 
 for(let attempts = 0; attempts < 4; attempts++){
-    let FIRST_NUMBER = +(prompt('Input first number.', ''));
-    let SECOND_NUMBER = +(prompt('Input second number.', ''));
+    let first = +(prompt('Input first number.', ''));
+    let second = +(prompt('Input second number.', ''));
 
-        if(isNaN(FIRST_NUMBER) && isNaN(SECOND_NUMBER)){
+        if(isNaN(first) && isNaN(second)){
             alert('We do not have number, input again!');
-        } else if(FIRST_NUMBER % 1 != 0 && SECOND_NUMBER % 1 != 0){ //Перевірте, чи є передане значення (N,M) ЦІЛИМ числом.
+        } else if(first % 1 != 0 && second % 1 != 0){ //Перевірте, чи є передане значення (N,M) ЦІЛИМ числом.
             alert('We do not have integer number, input again!');
-        } else if(isNaN(FIRST_NUMBER)){
-            alert('Your sum = ' + SECOND_NUMBER)
-        } else if(isNaN(SECOND_NUMBER)){
-            alert('Your sum = ' + FIRST_NUMBER)
-        } else {    const n = parseInt(FIRST_NUMBER, 10);
-                    const m = parseInt(SECOND_NUMBER, 10);
+        } else if(isNaN(first)){
+            alert('Your sum = ' + second)
+        } else if(isNaN(second)){
+            alert('Your sum = ' + first)
+        } else {    const n = parseInt(first, 10);
+                    const m = parseInt(second, 10);
                     console.log("Your number: " + n);
                     document.writeln("Your number: " + n);
                     console.log("Your number: " + m);
