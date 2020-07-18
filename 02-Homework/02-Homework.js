0ed4b39861ea38a2b6fb13a0c7d869dfaf635a90
Vlad Huliaiev <gulyaev4420@gmail.com>
2020-07-18 19:25:48 +0300

    refactpring

--- 02-Homework/02-Homework.js
+++ 02-Homework/02-Homework.js
@@ -6,27 +6,25 @@ for(let attempts = 0; attempts < 4; attempts++){
     let second = +(prompt('Input second number.', ''));
 
         if(isNaN(first) && isNaN(second)){
-            alert('We do not have number, input again!');
-        } else if(first % 1 != 0 && second % 1 != 0){ //Перевірте, чи є передане значення (N,M) ЦІЛИМ числом.
-            alert('We do not have integer number, input again!');
-        } else if(isNaN(first)){
-            alert('Your sum = ' + second)
-        } else if(isNaN(second)){
-            alert('Your sum = ' + first)
+            alert('Only numbers are accepted!');
+        } else if(first % 1 != 0 && second % 1 != 0){
+            alert('Only integers are accepted!');
+        } else if(isNaN(first) || first % 1 != 0){
+            alert('Only integers are accepted!')
+        } else if(isNaN(second) || second % 1 != 0){
+            alert('Only integers are accepted!')
         } else {    const n = parseInt(first, 10);
                     const m = parseInt(second, 10);
-                    console.log("Your number: " + n);
-                    document.writeln("Your number: " + n);
-                    console.log("Your number: " + m);
-                    document.writeln("Your number: " + m);
+                    document.writeln("Your number first: " + first);
+                    document.writeln("Your number second: " + second);
+                    console.log("Your number first: " + n);
+                    console.log("Your number second: " + m);
                     //Отримайте у користувача число (N,M) від якого ми будемо складати.
 
                     const skipEvenNumbers = confirm('Whether to skip even numbers?');
                     if(skipEvenNumbers == true) {
-                        console.log('We must will skip even numbers!');
                         document.writeln('We are the skip even numbers!');
                     } else {
-                        console.log('We do not miss even numbers!');
                         document.writeln('Ok, we use all numbers!');
                     }   //Отримайте у користувача булевий параметр(true/false), який підкаже нам чи потрібно пропускати парні числа.
 
@@ -43,8 +41,9 @@ for(let attempts = 0; attempts < 4; attempts++){
                                 }
 
                             }
-                            console.log(sum);
+
                         }
+                        console.log(sum);
                         document.writeln('Sum: ' + sum);
 
                     } else if(n > m){
@@ -57,7 +56,9 @@ for(let attempts = 0; attempts < 4; attempts++){
                                     if(i % 2 != 0){ //пропускає парні числа
                                     sum += i;
                                     }
+
                                 }
+
                             }
                             document.writeln('Sum: ' + sum);
 
@@ -73,6 +74,9 @@ for(let attempts = 0; attempts < 4; attempts++){
                                     let sum = 0;
                                     sum = n + m;
                                     document.writeln('Sum: ' + sum);
+                                }
+                                if(i % 2 == 0 && n != 0 && m != 0){
+                                document.writeln('You sum is: ' + 0);
                                 }
                                 if(n + m == 0){
                                     document.writeln('You sum is: ' + 0);
