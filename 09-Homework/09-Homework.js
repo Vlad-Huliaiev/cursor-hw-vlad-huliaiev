7b3f3b081f2ae4fe0635dfc18660dce5ee7117d3
Vlad Huliaiev <gulyaev4420@gmail.com>
2020-08-17 14:36:02 +0300

    09-Homework added

---
+++ 09-Homework/09-Homework.js
@@ -0,0 +1,26 @@
+
+function generateBlocks() {
+    let square25 = document.querySelector('.square25');
+    let block;
+
+    for( let i = 0; i < 5 ; i++ ) {
+        for( let j = 0; j < 5 ; j++ ) {
+            block = document.createElement('div');
+
+            block.style.backgroundColor = 'rgb('+getRandomInt(0,255)+', '+getRandomInt(0,255)+', '+getRandomInt(0,255)+')';
+
+            square25.appendChild(block);
+        }
+    }
+}
+
+generateBlocks();
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+/*
+function generateColor(b) {
+    b.style.backgroundColor = 'rgb('+getRandomInt(0,255)+', '+getRandomInt(0,255)+', '+getRandomInt(0,255)+')';
+}
+*/
