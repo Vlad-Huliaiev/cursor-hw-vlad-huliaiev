10a8d79d74456739ebe4056d6616ebc1856c8d75
Vlad Huliaiev <gulyaev4420@gmail.com>
2020-09-14 14:00:46 +0300

    re-changes

---
+++ 15-Homework/reactjs-start/build/precache-manifest.2abfd642efa2db95915790ed5f7575dc.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "178501d5ec87e116c1cbc673ef502048",
+    "url": "/cursor-hw-vlad-huliaiev/index.html"
+  },
+  {
+    "revision": "9c8d213d6f4151fb8882",
+    "url": "/cursor-hw-vlad-huliaiev/static/css/main.695b2db4.chunk.css"
+  },
+  {
+    "revision": "2203d5605cbb457f0182",
+    "url": "/cursor-hw-vlad-huliaiev/static/js/2.dd3ed177.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cursor-hw-vlad-huliaiev/static/js/2.dd3ed177.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c8d213d6f4151fb8882",
+    "url": "/cursor-hw-vlad-huliaiev/static/js/main.073a7211.chunk.js"
+  },
+  {
+    "revision": "55747ee5253b48a34e68",
+    "url": "/cursor-hw-vlad-huliaiev/static/js/runtime-main.1e07d3d8.js"
+  },
+  {
+    "revision": "18867fcd477d88985ae98d33173185b6",
+    "url": "/cursor-hw-vlad-huliaiev/static/media/anakin_img.18867fcd.jpg"
+  },
+  {
+    "revision": "5b072750bef0d49b0587d3a1f990c5fd",
+    "url": "/cursor-hw-vlad-huliaiev/static/media/background.5b072750.jpg"
+  },
+  {
+    "revision": "3352fdeb8e3413067df241dbfdcd4031",
+    "url": "/cursor-hw-vlad-huliaiev/static/media/leia_img.3352fdeb.jpg"
+  },
+  {
+    "revision": "55da1bf0166a5eac98ee67e20a1ac573",
+    "url": "/cursor-hw-vlad-huliaiev/static/media/luke_img.55da1bf0.jpg"
+  },
+  {
+    "revision": "40b50c9e452e56b79e8bff1241aa3d54",
+    "url": "/cursor-hw-vlad-huliaiev/static/media/palpatine.40b50c9e.jpg"
+  },
+  {
+    "revision": "22de823b055e4f6b460f6a86c66042e8",
+    "url": "/cursor-hw-vlad-huliaiev/static/media/rey_avatar.22de823b.jpg"
+  },
+  {
+    "revision": "50847147924e88d4d429dc887b29341a",
+    "url": "/cursor-hw-vlad-huliaiev/static/media/rey_img.50847147.jpg"
+  },
+  {
+    "revision": "718524087d6534674e0793834348b97b",
+    "url": "/cursor-hw-vlad-huliaiev/static/media/rey_img_2.71852408.jpg"
+  },
+  {
+    "revision": "28584aa096f1429d5c4d19af7957e630",
+    "url": "/cursor-hw-vlad-huliaiev/static/media/rey_img_3.28584aa0.jpg"
+  },
+  {
+    "revision": "9febcbf7b40a922eaadf202a874fae14",
+    "url": "/cursor-hw-vlad-huliaiev/static/media/tick1.9febcbf7.png"
+  }
+]);
